157e1280da13d9a6ab67891433c31b6e9d44d148
App Academy Student <student@appacademy.io>
2013-09-26 12:22:33 -0700

    Implements jump_moves

--- Piece.rb
+++ Piece.rb
@@ -20,7 +20,7 @@ class Piece
       end
     else#even row
       moves = [pos-4, pos-3, pos+4, pos+5]
-      if pos > 28 #top of board
+      if pos > 28 #bottom of board
         moves.delete(pos+4, pos+5)
       end
 
@@ -42,6 +42,33 @@ class Piece
   end
 
   def jump_moves(piece_pos)
+    moves = [pos-9, pos-7, pos+7, pos+9]
+
+    if pos < 9#top two rows of board
+      moves.delete(pos-4, pos-3)
+    end
+    if pos > 24 #bottom two rows of board
+      moves.delete(pos+4, pos+5)
+    end
+
+    #Odd rows should not appear at left end of board, so just remove right ones
+
+    if col_end(pos) < 0#left end
+      moves.delete(pos+7)
+      moves.delete(pos-9)
+    elsif col_end(pos) > 0#right end
+      moves.delete(pos-7)
+      moves.delete(pos+9)
+    end
+
+    #remove back dir slides if not promoted
+    if !promoted
+      if color == :white#white moves up
+        moves.delete_if {|num| num > piece_pos}
+      else#black moves down
+        moves.delete_if {|num| num < piece_pos}
+      end
+    end
   end
 
   def row_num(pos)
@@ -49,7 +76,9 @@ class Piece
   end
   def col_end(pos)
     return -1 if [5,13,21,29].include?(pos)
+    return -2 if [1,9,17,25].include?(pos)
     return 1 if [4,12,20,28].include?(pos)
+    return 2 if [8,16,24,32].include?(pos)
     return 0
   end
 end
